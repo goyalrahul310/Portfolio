a5c24936e1f43f2c7311d2defe18f0fa06f1229f
rahul <guptarahul3100@gmail.com>
2020-05-29 02:28:30 +0530

    portfolio

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+	context = {}
+	return render(request,'main/index.html',context)
+
+
+def projects(request):
+	context = {}
+	return render(request,'main/project.html',context)
+
+def languages(request):
+	context = {}
+	return render(request,'main/languages.html',context)
